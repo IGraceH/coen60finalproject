94fe2749517777b2d86eb67e9914e905a9242999
Megu Kanzawa <113965829+MeguKanzawa@users.noreply.github.com>
2023-12-01 17:31:17 -0800

    Added filter functionality classes to elements
    Not completely done yet

--- get_info.php
+++ get_info.php
@@ -1,5 +1,6 @@
 <?php
     $file = "studentList.txt";
+    // $debugF = "debugFile.txt";
     $currentEntry = trim($_GET["info"]); // current entry in string format
     $currentEntryList = explode(",", trim($_GET["info"])); // current entry in list format
 
@@ -9,10 +10,11 @@
     
     // go through each line from file, check if there are any duplicate emails
     foreach ($contents as $lines) {
-        $return_str .= "<tr>";
         $one = explode(",", trim($lines));
+        $major = $one[2];
+        $return_str .= "<tr class=" . $major . ">";
         foreach ($one as $elem) {
-            $return_str .= "<td>" . $elem . "</td>";
+            $return_str .= "<td class=\"" . $major . " student-table-items\">" . $elem . "</td>";
         }
         if ($one[3] == $currentEntryList[3]) {
             $duplicate = true;
@@ -23,15 +25,17 @@
     // if a duplicated email is not found, add current entry to table & text file
     // otherwise, skip the line and return the current file contents
     if ($duplicate == false) {
-        $return_str .= "<tr>";
+        $major = $currentEntryList[2];
+        $return_str .= "<tr class=" . $major . ">";
         foreach ($currentEntryList as $items) {
-            $return_str .= "<td>" . $items . "</td>";
+            $return_str .= "<td class=\"" . $major . " student-table-items\">" . $items . "</td>";
         }
         $return_str .= "</tr>";
         file_put_contents($file, $currentEntry . "\n", FILE_APPEND);
     } 
     $return_str .= "</table>";
     
+    // file_put_contents($debugF, $return_str . "\n", FILE_APPEND);
     // return file contents + new entry (if not duplicate) in HTML table format
     echo $return_str;
 ?>
